eb8c1614da2dc63c7e6a832104477933c875604d
Nitish Kumar <kumarnitishbxr07@gmail.com>
2025-11-12 11:18:27 +0530

    Code uploaded

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+function App() {
+
+
+  return (
+    <>
+      
+      <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gray-400" >
+       <div className="bg-green-200 gap-6 w-1/2">
+        Hello Nitish Kumar
+        
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor non eius dolorem eveniet perferendis ab repellendus illum exercitationem omnis consectetur nisi aliquam explicabo, libero dicta ad ratione facere officia porro.
+      </p>
+      </div>
+      </section>
+      
+    </>
+  )
+}
+
+export default App
